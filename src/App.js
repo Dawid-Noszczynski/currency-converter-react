55ea3e1f2e017bba4b3f8031b77fdbf09df3bc2c
Dawid Noszczynski <dawid.noszczynski@icloud.com>
2023-02-13 15:48:51 +0100

    interaction added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import Header from "./Header";
 import Form from "./Form";
 import Footer from "./Footer";
+import { useState } from "react";
+import currencies from "./Currencies";
 
 function App() {
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies.find(({ name }) => name === currency).rate;
+
+    setResult({
+      targetAmount: amount / rate,
+      currency,
+    });
+  }
+
   return (
     <>
-    <Header title="currency converter"/>
-    <Form/>
-    <Footer title="made by dawid noszczynski | 2023"/>
+      <Header title="currency converter" />
+      <Form result={result} calculateResult={calculateResult} />
+      <Footer title="exchange rate from 13.02.2023." />
     </>
   );
 }
