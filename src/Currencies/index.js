e2ec110e7c73ba2c7b76b097b260215432a71709
Dawid Noszczynski <dawid.noszczynski@icloud.com>
2023-02-13 15:48:29 +0100

    currencies component added

---
+++ src/Currencies/index.js
@@ -0,0 +1,8 @@
+const currencies = [
+    { name: "USD", rate: 4.47 },
+    { name: "EUR", rate: 4.77 }, 
+    { name: "CHF", rate: 4.84 },
+    { name: "GBP", rate: 5.28 },
+];
+
+export default currencies;
