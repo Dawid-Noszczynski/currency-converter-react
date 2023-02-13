b276a5212da0eae0dc2c625c9e8b8c9e8d2d9c7d
Dawid Noszczynski <dawid.noszczynski@icloud.com>
2023-02-13 15:48:10 +0100

    result component added

---
+++ src/Result/index.js
@@ -0,0 +1,13 @@
+import "./style.css"
+
+const Result = ({ result }) => (
+    <p className="result">
+        {!!result && (
+            <strong>
+                {result.targetAmount.toFixed(2)} {result.currency}
+            </strong>
+        )}
+    </p>
+);
+
+export default Result;
